3ce394ca47abdc0c65c94b8f8d289d742096644c
reggmack <thegraphixninja@gmail.com>
2016-12-29 17:01:40 -0600

    parse-json
    Parse the JSON string

--- app.js
+++ app.js
@@ -2,36 +2,32 @@
 // Solution: Use Node.js to connect to Treehouse's API to get profile information to print out
 
 var https = require("https");
-var username = "chalkers";
+var username = "reggiemack";
 
 function printMessage(username, badgeCount, points) {
-	var message = username + "has " + badgeCount + " total badge(s) and " + points + " points in JavaScript";
+	var message = username + " has " + badgeCount + " total badge(s) and " + points + " points in JavaScript";
 	console.log(message);
 }
 
-
 //Connect to the API URL (http://teamtreehouse.com/username.json)
-	var request = https.get("https://teamtreehouse.com/" + username + ".json", function(response) {
+var request = https.get("https://teamtreehouse.com/" + username + ".json", function(response) {
 	var body = "";
-	
+
 	// Read the data
 	response.on('data', function (chunk) {
 	body += chunk;
-});
-	response.on('end', function() {
-		console.log(body);
-		console.log(typeof body);
 	});
-		
-		
+
+	response.on('end', function() {
+		var profile = JSON.parse(body);
+		printMessage(username, profile.badges.length, profile.points.JavaScript)
+	});
 });
-
-
 
 // Parse the data
 
 // Print the data
-// printMessage("Chalkers", 1000, 2000000);
+// printMessage("reggiemack", 1000, 2000000);
 
 request.on("error", function(error) {
 	console.error(error.message);
